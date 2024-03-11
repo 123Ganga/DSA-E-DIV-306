24e75281195a351427ac85981e4d04f81b0c82f7
123Ganga <120097764+123Ganga@users.noreply.github.com>
2024-03-11 21:15:41 +0530

    Add files via upload

---
+++ pointer 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    float f;
+    char c;
+    double d;
+
+    int *pnum;
+    float *fp;
+    char *cp;
+    double *dp;
+
+    printf("size of n is %d bytes \n",size of(int));
+    printf("size of n is %f bytes \n",size of(float));
+    printf("size of n is %c bytes \n",size of(char));
+    printf("size of n is % bytes \n",size of(int));
+
+
+
